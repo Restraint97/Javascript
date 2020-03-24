becefd009bbc6cde514492bed612ee5d07fe2613
Restraint97 <nathanyosef@gmail.com>
2020-03-24 10:23:05 +0100

    work

---
+++ baseinn.js
@@ -0,0 +1,41 @@
+// function word() {
+
+//     var wordcounter = 70;
+
+//     document.getElementsByClassName("block")[0].style = "display: none;"
+//     document.getElementsByClassName("hallo")[0].style = "display: block;"
+//     document.getElementsByClassName("hallo")[0].classList.toggle("show-square");
+
+
+// }
+
+
+// .block {
+//     background-color: black;
+//     position: relative;
+//     border-radius: 70px;
+//     top: 12rem;
+//     left: calc(50% - 50px);
+//     display: flex;
+//     border: 1px solid;
+//     width: 10rem;
+//     height: 10rem;
+// }
+
+// .hallo {
+//     visibility: hidden;
+//     left: calc(50% - 50px);
+//     position: relative;
+//     top: 12rem;
+//     border-radius: 70px;
+//     border: 1px solid;
+//     width: 10rem;
+//     height: 10rem;
+//     background-color: blueviolet;
+//     transition: all ease 3s;
+// }
+
+// .show-square {
+//     border-radius: 0;
+//     visibility: visible;
+// }
